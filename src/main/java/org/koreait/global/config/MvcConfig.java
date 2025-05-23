c477f8a26962bc7978d45777db2ef26a6ded8075
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-05-23 16:15:44 +0900

    '초기ㅣ'

---
+++ src/main/java/org/koreait/global/config/MvcConfig.java
@@ -0,0 +1,7 @@
+package org.koreait.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class MvcCofig implements WebMvcConfigurer {
+}
